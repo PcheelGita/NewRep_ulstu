f44997a812ef0fa00d9269eb1461e210518e31fd
Pcheel_Gita <mrghostgamesi@gmail.com>
2025-12-12 09:46:41 +0400

    Еще один коммит. СРР файл исчезает?!

---
+++ LastTimeLab4/LastTimeLab4/Poteryan.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Введите целое число: ");
+    scanf_s("%d", &number);
+
+    printf("Остаток от деления на 3: %d\n", number % 3);
+
+    return 0;
+}
